2a4f7d807b39c679139123db5acbe56d9b2138d6
Thomas Staudinger <Staudi.Kaos@gmail.com>
2018-11-08 23:27:56 +0100

    Fix Skype tray icon

--- network/im/skype/actions.py
+++ network/im/skype/actions.py
@@ -13,6 +13,11 @@ def setup():
     shelltools.system("pwd")
     shelltools.system("ar xf skypeforlinux_%s_amd64.deb" % Version)
     shelltools.system("tar xf data.tar.xz")
+    shelltools.system("npm install -g asar")
+    shelltools.system("asar extract usr/share/skypeforlinux/resources/app.asar new_app")
+    shelltools.system(' ls -1 new_app/images/tray/linux | grep "@2x" | while read -r pngFile; do cp "new_app/images/tray/linux/$pngFile" "new_app/images/tray/linux/${pngFile//$@@2x/}"; done')
+    shelltools.system("asar pack new_app app.asar")
+    shelltools.system("cp app.asar usr/share/skypeforlinux/resources/app.asar")
 
 def install():
     pisitools.insinto("/", "usr")
