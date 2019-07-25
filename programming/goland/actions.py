fc3184f5be6d9a776669541b0b36ec29b2dcc47e
James Lee <jamesl33info@gmail.com>
2019-07-25 19:32:51 +0100

    Update Goland to version 2019.2

--- programming/goland/actions.py
+++ programming/goland/actions.py
@@ -7,6 +7,6 @@ WorkDir = "."
 Version =  get.srcVERSION()
 
 def install():
-    shutil.rmtree("GoLand-%s/jre64" % Version)
+    shutil.rmtree("GoLand-%s/jbr" % Version)
     pisitools.insinto("/opt/goland", "GoLand-*/*")
     pisitools.dosym("/opt/goland/bin/goland.sh", "/usr/bin/goland")
