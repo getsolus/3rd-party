c886ecea9eee4cb49f8386b214b1b1c074f57ba8
ahahn94 <ahahn94@outlook.com>
2019-07-27 01:32:39 +0200

    Update PhpStorm 2019.2
    Update PhpStorm to version 2019.2 from July 25, 2019.
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- programming/phpstorm/actions.py
+++ programming/phpstorm/actions.py
@@ -4,9 +4,8 @@ from pisi.actionsapi import get, pisitools, shelltools
 import shutil
 
 WorkDir = "."
-Build = "191.7479.51"
+Build = "192.5728.108"
 
 def install():
-    shutil.rmtree("PhpStorm-%s/jre64" % Build)
     pisitools.insinto("/opt/phpstorm", "PhpStorm-%s/*" % Build)
     pisitools.dosym("/opt/phpstorm/bin/phpstorm.sh", "/usr/bin/phpstorm")
