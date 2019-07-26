acdfadbc63cf82ac73c311372fb22491497ac959
ahahn94 <ahahn94@outlook.com>
2019-07-27 01:41:03 +0200

    Update WebStorm 2019.2
    Update WebStorm to version 2019.2 from July 22, 2019.
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- programming/webstorm/actions.py
+++ programming/webstorm/actions.py
@@ -4,9 +4,8 @@ from pisi.actionsapi import get, pisitools, shelltools
 import shutil
 
 WorkDir = "."
-Build = "191.7479.14"
+Build = "192.5728.87"
 
 def install():
-    shutil.rmtree("WebStorm-%s/jre64" % Build)
     pisitools.insinto("/opt/webstorm", "WebStorm-%s/*" % Build)
     pisitools.dosym("/opt/webstorm/bin/webstorm.sh", "/usr/bin/webstorm")
