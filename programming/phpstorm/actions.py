b33b98cccb107c836405986a7d0be0f741f81585
ahahn94 <ahahn94@outlook.com>
2019-11-16 13:15:39 +0100

    Update PhpStorm 2019.2.5
    Update PhpStorm to version 2019.2.5 from 13.11.2019.
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- programming/phpstorm/actions.py
+++ programming/phpstorm/actions.py
@@ -4,7 +4,7 @@ from pisi.actionsapi import get, pisitools, shelltools
 import shutil
 
 WorkDir = "."
-Build = "192.7142.41"
+Build = "192.7142.51"
 
 def install():
     pisitools.insinto("/opt/phpstorm", "PhpStorm-%s/*" % Build)
