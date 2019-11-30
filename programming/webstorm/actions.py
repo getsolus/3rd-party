db5b50808c1456c7bd4a31cef7def7f1139d669e
ahahn94 <ahahn94@outlook.com>
2019-11-30 19:26:38 +0100

    Update WebStorm 2019.3
    Update WebStorm to version 2019.3 from 25.11.2019.
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- programming/webstorm/actions.py
+++ programming/webstorm/actions.py
@@ -4,7 +4,7 @@ from pisi.actionsapi import get, pisitools, shelltools
 import shutil
 
 WorkDir = "."
-Build = "192.7142.35"
+Build = "193.5233.80"
 
 def install():
     pisitools.insinto("/opt/webstorm", "WebStorm-%s/*" % Build)
