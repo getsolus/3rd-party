3bc9e1e7372944dd4210d70d320d45710aff0e90
ahahn94 <ahahn94@outlook.com>
2020-04-10 22:08:08 +0200

    Update WebStorm 2020.1
    Update WebStorm to version 2020.1 from 7 April 2020.
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- programming/webstorm/actions.py
+++ programming/webstorm/actions.py
@@ -4,7 +4,7 @@ from pisi.actionsapi import get, pisitools, shelltools
 import shutil
 
 WorkDir = "."
-Build = "193.6911.28"
+Build = "201.6668.106"
 
 def install():
     pisitools.insinto("/opt/webstorm", "WebStorm-%s/*" % Build)
