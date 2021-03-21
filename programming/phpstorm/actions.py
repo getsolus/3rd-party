9c88f47889cddb1b9fa1e58d43830319565954c9
ahahn94 <ahahn94@outlook.com>
2021-03-22 00:34:27 +0100

    Update PhpStorm 2020.3.3
    Update PhpStorm to version 2020.3.3 from 16 March 2021.
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- programming/phpstorm/actions.py
+++ programming/phpstorm/actions.py
@@ -4,7 +4,7 @@ from pisi.actionsapi import get, pisitools, shelltools
 import shutil
 
 WorkDir = "."
-Build = "203.7148.74"
+Build = "203.7717.64"
 
 def install():
     pisitools.insinto("/opt/phpstorm", "PhpStorm-%s/*" % Build)
