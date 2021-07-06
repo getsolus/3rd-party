950d597b735bfabe7bde46921ee8491a03bf04f4
ahahn94 <ahahn94@outlook.com>
2021-07-06 17:25:22 +0200

    Update WebStorm 2021.1.3
    Update WebStorm to version 2021.1.3 from 1 July 2021.
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- programming/webstorm/actions.py
+++ programming/webstorm/actions.py
@@ -4,7 +4,7 @@ from pisi.actionsapi import get, pisitools, shelltools
 import shutil
 
 WorkDir = "."
-Build = "211.7442.26"
+Build = "211.7628.25"
 
 def install():
     pisitools.insinto("/opt/webstorm", "WebStorm-%s/*" % Build)
