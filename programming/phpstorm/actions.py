892251b9527ea9447b3036559c6d460ec17e43a2
ahahn94 <ahahn94@outlook.com>
2021-07-31 17:36:32 +0200

    Update PhpStorm 2021.2
    Update PhpStorm to version 2021.2 from 29 July 2021.
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- programming/phpstorm/actions.py
+++ programming/phpstorm/actions.py
@@ -4,7 +4,7 @@ from pisi.actionsapi import get, pisitools, shelltools
 import shutil
 
 WorkDir = "."
-Build = "211.7628.25"
+Build = "212.4746.100"
 
 def install():
     pisitools.insinto("/opt/phpstorm", "PhpStorm-%s/*" % Build)
