eb1e8b1cc0876ec25e14149cce485c4fb6b7b142
Thomas Staudinger <Staudi.Kaos@gmail.com>
2022-01-15 06:19:08 +0100

    Fix scrivener launch (freetype error)

--- office/scrivener/actions.py
+++ office/scrivener/actions.py
@@ -15,6 +15,7 @@ def setup():
     shelltools.system("tar xf data.tar.gz")
     shelltools.system("mkdir usr/share/icons/hicolor")
     pisitools.move("usr/share/icons/*x*", "usr/share/icons/hicolor/")
+    shelltools.system("rm usr/share/scrivener/bin/scrivener.sh")
 
 def install():
     pisitools.insinto("/", "usr")
