d140dd5b2365ff80003b2f43629842bce65c37e1
Thomas Staudinger <Staudi.Kaos@gmail.com>
2022-04-16 20:44:40 +0200

    Update Android Studio to 2021.1.1.23

--- programming/android-studio/actions.py
+++ programming/android-studio/actions.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
 
-from pisi.actionsapi import get, pisitools, shelltools
-
-def setup():
-    shelltools.system("tar xf android-studio-%s-linux.tar.gz" % get.srcVERSION())
+from pisi.actionsapi import get, pisitools
 
 def install():
-    pisitools.insinto("/opt/android-studio", "android-studio/*")
+    pisitools.insinto("/opt/android-studio", "*")
     pisitools.dosym("/opt/android-studio/bin/studio.sh", "/usr/bin/android-studio")
