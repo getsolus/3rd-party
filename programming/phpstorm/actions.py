becae2fc4074df90a808ca44baf0e77fc5b3077b
ahahn94 <ahahn94@outlook.com>
2022-12-11 22:43:47 +0100

    Update PhpStorm to version 2022.3
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- programming/phpstorm/actions.py
+++ programming/phpstorm/actions.py
@@ -4,7 +4,7 @@ from pisi.actionsapi import get, pisitools, shelltools
 import shutil
 
 WorkDir = "."
-Build = "222.4459.28"
+Build = "223.7571.212"
 
 def install():
     pisitools.insinto("/opt/phpstorm", "PhpStorm-%s/*" % Build)
