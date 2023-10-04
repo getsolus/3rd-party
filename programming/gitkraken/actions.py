b6f68c9ff3b2e9ad295818554771b20439df943e
Thomas Staudinger <Staudi.Kaos@gmail.com>
2023-10-04 23:26:50 +0200

    Update GitKraken to 9.9.0

--- programming/gitkraken/actions.py
+++ programming/gitkraken/actions.py
@@ -13,7 +13,7 @@ Suffix = "-1"
 def setup():
     shelltools.system("pwd")
     shelltools.system("ar xf gitkraken-amd64.deb")
-    shelltools.system("tar xJvf data.tar.xz")
+    shelltools.system("tar xvf data.tar.zst")
     shelltools.system("sed -i 's Icon=app Icon=gitkraken ' usr/share/applications/gitkraken.desktop")
 def install():
     pisitools.insinto("/", "usr")
