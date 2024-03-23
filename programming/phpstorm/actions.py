032914328f511cb2f3f545ef71e2607ec6db77f1
ahahn94 <ahahn94@outlook.com>
2024-03-23 22:00:04 +0100

    Update PhpStorm to version 2023.3.6
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- programming/phpstorm/actions.py
+++ programming/phpstorm/actions.py
@@ -4,7 +4,7 @@ from pisi.actionsapi import get, pisitools, shelltools
 import shutil
 
 WorkDir = "."
-Build = "233.14808.18"
+Build = "233.15026.12"
 
 def install():
     pisitools.insinto("/opt/phpstorm", "PhpStorm-%s/*" % Build)
