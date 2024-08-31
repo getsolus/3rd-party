d0511b5eeab72e3beb08e559db1f0a24888ae984
ahahn94 <ahahn94@outlook.com>
2024-08-31 14:36:25 +0200

    Update PhpStorm to version 2024.2.1
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- programming/phpstorm/actions.py
+++ programming/phpstorm/actions.py
@@ -4,7 +4,7 @@ from pisi.actionsapi import get, pisitools, shelltools
 import shutil
 
 WorkDir = "."
-Build = "242.20224.361"
+Build = "242.21829.154"
 
 def install():
     pisitools.insinto("/opt/phpstorm", "PhpStorm-%s/*" % Build)
