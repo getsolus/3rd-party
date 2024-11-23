35e760a163094feeee1ee66e79bed3b4638a2bbc
ahahn94 <ahahn94@outlook.com>
2024-11-23 18:14:23 +0100

    Update CLion to version 2024.3
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- programming/clion/actions.py
+++ programming/clion/actions.py
@@ -8,4 +8,4 @@ Version =  get.srcVERSION()
 
 def install():
     pisitools.insinto("/opt/clion", "clion-%s/*" % Version)
-    pisitools.dosym("/opt/clion/bin/clion.sh", "/usr/bin/clion")
+    pisitools.dosym("/opt/clion/bin/clion", "/usr/bin/clion")
