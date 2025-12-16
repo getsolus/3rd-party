c8fe74292ed54ecc4f595b009e9068455da1e357
ahahn94 <ahahn94@outlook.com>
2025-12-16 16:29:03 +0100

    Update WebStorm to version 2025.3
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- programming/webstorm/actions.py
+++ programming/webstorm/actions.py
@@ -4,7 +4,7 @@ from pisi.actionsapi import get, pisitools, shelltools
 import shutil
 
 WorkDir = "."
-Build = "252.28238.10"
+Build = "253.28294.332"
 
 def install():
     pisitools.insinto("/opt/webstorm", "WebStorm-%s/*" % Build)
